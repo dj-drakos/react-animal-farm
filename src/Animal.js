45c4bf356e8730ff74848bbd64b281aa6a44b2e1
DJ Drakos <dianajodrakos@gmail.com>
2022-01-25 16:17:17 -0800

    app styling

--- src/Animal.js
+++ src/Animal.js
@@ -1,5 +1,5 @@
 export default function Animal({ name, type, says, top, left }) {
-  return <section style={{ bottom: top, left: left }}>
+  return <section style={{ top: top, left: left }}>
     <img src={`/images/${type}.svg`} alt={type} />
     <h4>{name}</h4>
     <p>{says}</p>
--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer(props) {
   return (
     <footer>
-      <div>Copyright Alchemy Code Lab 2021-2022</div>
       <div><a href={`mailto:${props.email}`}>Contact the Farm</a></div>
+      <div>Copyright Alchemy Code Lab 2021-2022</div>
     </footer>
   );
 }
